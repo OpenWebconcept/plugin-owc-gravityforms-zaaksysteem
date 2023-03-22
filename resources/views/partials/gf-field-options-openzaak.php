2c2982145e16b04d723136930db9354f31e580fa
Robert Bossaert <contact@robertbossaert.com>
2023-03-22 14:50:26 +0100

    (chore): add standard oz options

--- resources/views/partials/gf-field-options-openzaak.php
+++ resources/views/partials/gf-field-options-openzaak.php
@@ -2,6 +2,7 @@
 
 use function OWC\Zaaksysteem\Foundation\Helpers\config;
 
+// TODO: check if we can get this list from the OZ api.
 ?>
 <li class="label_setting field_setting">
     <label for="linkedField" class="section_label">
@@ -9,5 +10,12 @@ use function OWC\Zaaksysteem\Foundation\Helpers\config;
     </label>
     <select id="linkedField" onchange="SetFieldProperty('linkedFieldValue', this.value);">
         <option value=""><?php _e('Kies veldnaam OpenZaak', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('bronorganisatie', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('omschrijving', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('toelichting', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('registratiedatum', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('verantwoordelijkeOrganisatie', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('startdatum', config('core.text_domain')) ?></option>
+        <option value=""><?php _e('any', config('core.text_domain')) ?></option>
     </select>
 </li>
