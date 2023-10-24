4dd9cec70b2cba4e8426dd07b53f7a67d9bd684c
Mike van den Hoek <mike@yard.nl>
2023-10-24 18:38:51 +0200

    (refactor): clean-up Authenticators

--- src/Clients/OpenZaak/Authenticator.php
+++ src/Clients/OpenZaak/Authenticator.php
@@ -21,11 +21,11 @@ class Authenticator extends AbstractTokenAuthenticator
     public function generateToken(): string
     {
         $payload = [
-            'iss'                   => $this->clientId,
-            'iat'                   => time(),
-            'client_id'             => $this->clientId,
-            'user_id'               => $this->clientId,
-            'user_representation'   => $this->clientId,
+            'iss' => $this->clientId,
+            'iat' => time(),
+            'client_id' => $this->clientId,
+            'user_id' => $this->clientId,
+            'user_representation' => $this->clientId,
         ];
 
         return JWT::encode($payload, $this->clientSecret, 'HS256');
