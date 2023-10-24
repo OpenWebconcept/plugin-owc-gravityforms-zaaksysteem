f2318ef92a37245c6fc02574a229b62961a098e1
Mike van den Hoek <mike@yard.nl>
2023-10-24 18:37:49 +0200

    (feat): retrieve 'zaken' combined from all suppliers

--- src/Blocks/Zaken/Block.php
+++ src/Blocks/Zaken/Block.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace OWC\Zaaksysteem\Blocks\Zaken;
 
+use Exception;
 use OWC\Zaaksysteem\Contracts\Client;
 use OWC\Zaaksysteem\Endpoints\Filter\ResultaattypenFilter;
 use OWC\Zaaksysteem\Endpoints\Filter\ZakenFilter;
@@ -20,13 +21,22 @@ class Block
 
     public function render($attributes, $rendered, $editor)
     {
+        // Bail early when in editor.
+        if (is_admin()) {
+            return;
+        }
+
         $this->client = ContainerResolver::make()->getApiClient($attributes['zaakClient'] ?? 'openzaak');
 
         if (! $this->client->supports('zaken')) {
             return 'Het Mijn Zaken overzicht is niet beschikbaar.';
         }
 
-        $zaken = $this->getZaken($attributes);
+        if (! $attributes['combinedClients']) {
+            $zaken = $this->getZaken($attributes);
+        } else {
+            $zaken = $this->getCombinedZaken($attributes);
+        }
 
         if ($zaken->isEmpty()) {
             return 'Er zijn op dit moment geen zaken beschikbaar.';
@@ -49,6 +59,35 @@ class Block
         return $this->client->zaken()->filter($filter);
     }
 
+    protected function getCombinedZaken(array $attributes): Collection
+    {
+        $zaken = [];
+        $suppliers = ContainerResolver::make()->get('config')->get('suppliers');
+
+        foreach (array_keys($suppliers) as $supplier) {
+            $client = ContainerResolver::make()->getApiClient($supplier);
+            $filter = new ZakenFilter();
+            $filter = $this->handleFilterBSN($filter, $attributes);
+            $filter = $this->handleFilterZaaktype($filter, $attributes, $client);
+
+            try {
+                $zaken[] = $client->zaken()->filter($filter)->all();
+            } catch(Exception $e) {
+                continue;
+            }
+        }
+
+        return Collection::collect($zaken)->flattenAndAssign(function ($carry, $item) {
+            if (is_array($item)) {
+                return array_merge($carry, $item);
+            }
+
+            $carry[] = $item;
+
+            return $carry;
+        }, []);
+    }
+
     protected function handleFilterBSN(ZakenFilter $filter, array $attributes): ZakenFilter
     {
         if (! $attributes['byBSN']) {
@@ -61,7 +100,7 @@ class Block
         return $filter;
     }
 
-    protected function handleFilterZaaktype(ZakenFilter $filter, array $attributes): ZakenFilter
+    protected function handleFilterZaaktype(ZakenFilter $filter, array $attributes, ?Client $client = null): ZakenFilter
     {
         if (! is_string($attributes['zaaktypeFilter'])) {
             return $filter;
@@ -73,7 +112,7 @@ class Block
             return $filter;
         }
 
-        foreach ($this->zaaktypeIdentificationsToURL($identifications) as $zaaktype) {
+        foreach ($this->zaaktypeIdentificationsToURL($identifications, $client) as $zaaktype) {
             $filter->add('zaaktype', $zaaktype);
             // $filter->add('identificatie', $zaaktype); // Is not supported yet.
         }
@@ -86,13 +125,15 @@ class Block
      * This method should be removed when filtering on 'Zaaktype' identification is supported.
      * Don't forget to unuse this method in the foreach above as well.
      */
-    protected function zaaktypeIdentificationsToURL(array $identifications): array
+    protected function zaaktypeIdentificationsToURL(array $identifications, ?Client $client = null): array
     {
         $page = 1;
         $zaaktypen = [];
 
+        $client = $client ? $client : $this->client; // Use $client when 'zaken' from all the suppliers are retrieved combined.
+
         while ($page) {
-            $result = $this->client->zaaktypen()->all((new ResultaattypenFilter())->page($page));
+            $result = $client->zaaktypen()->all((new ResultaattypenFilter())->page($page));
             $zaaktypen = array_merge($zaaktypen, $result->all());
             $page = $result->pageMeta()->getNextPageNumber();
         }
