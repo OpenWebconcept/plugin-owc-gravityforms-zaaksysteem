17f9efd1c262ed84ec2e94644d6cc7ab8e09206b
Mike van den Hoek <mike@yard.nl>
2023-12-05 08:25:57 +0100

    (feat): validate if information object is allowed to be displayed

--- src/Entities/Casts/Related/Zaakinformatieobjecten.php
+++ src/Entities/Casts/Related/Zaakinformatieobjecten.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace OWC\Zaaksysteem\Entities\Casts\Related;
 
+use function OWC\Zaaksysteem\Foundation\Helpers\resolve;
 use OWC\Zaaksysteem\Endpoints\Filter\ZaakinformatieobjectenFilter;
 use OWC\Zaaksysteem\Entities\Enkelvoudiginformatieobject;
 use OWC\Zaaksysteem\Entities\Entity;
 use OWC\Zaaksysteem\Entities\Zaakinformatieobject;
-use function OWC\Zaaksysteem\Foundation\Helpers\resolve;
 
 use OWC\Zaaksysteem\Support\Collection;
 
@@ -22,7 +22,11 @@ class Zaakinformatieobjecten extends ResourceCollection
         $objects = $statussenEndpoint->filter($filter->byZaak($entity));
 
         return $objects->filter(function ($object) {
-            return $object instanceof Zaakinformatieobject && $object->informatieobject instanceof Enkelvoudiginformatieobject;
+            if (! $object instanceof Zaakinformatieobject || ! $object->informatieobject instanceof Enkelvoudiginformatieobject) {
+                return false;
+            }
+
+            return ! $object->informatieobject->isClassified() && $object->informatieobject->hasFinalStatus();
         });
     }
 }
