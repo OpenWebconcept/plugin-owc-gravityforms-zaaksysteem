67fd21750a8a409b705d7ee94a41f88c8200461b
Mike van den Hoek <mike@yard.nl>
2023-12-06 12:59:24 +0100

    (feat): comparing statusExplanation with zaakStatusExplanation in lowercase

--- resources/views/blocks/mijn-zaken/overview-zaken.php
+++ resources/views/blocks/mijn-zaken/overview-zaken.php
@@ -6,10 +6,10 @@ use function OWC\Zaaksysteem\Foundation\Helpers\view;
 <?php foreach ($vars['zaken'] as $zaak) : ?>
     <div class="zaak" data-identifier="<?= $zaak->uuid; ?>">
         <?php
-        echo view('blocks/mijn-zaken/zaak-collapse-button.php', [
-            'title' => $zaak->title(),
-            'id' => $zaak->identificatie,
-        ]);
+            echo view('blocks/mijn-zaken/zaak-collapse-button.php', [
+                'title' => $zaak->title(),
+                'id' => $zaak->identificatie,
+            ]);
     ?>
         <div class="collapse" id="collapse-<?php echo $zaak->identificatie; ?>">
             <div class="zaak-content">
