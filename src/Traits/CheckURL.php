39098aa43887ffb653b6dba1336581b18e445f46
Mike van den Hoek <mike@yard.nl>
2024-01-25 16:01:01 +0100

    (feat): add trait CheckURL

---
+++ src/Traits/CheckURL.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace OWC\Zaaksysteem\Traits;
+
+trait CheckURL
+{
+    public function checkURL($url, $test = false): bool
+    {
+        if (! $this->isValidUrl($url)) {
+            return false;
+        }
+
+        $response = wp_remote_get($url);
+
+        if (is_wp_error($response) || wp_remote_retrieve_response_code($response) !== 200) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function isValidUrl($url): bool
+    {
+        $url = filter_var($url, FILTER_SANITIZE_URL); // Remove invisible characters such as 'soft hyphens'.
+
+        return filter_var($url, FILTER_VALIDATE_URL) !== false;
+    }
+}
