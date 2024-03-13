eafefe213089e6506d2387559e36d9fe87718e20
Sander de Kroon <sanderdekroon@live.nl>
2024-03-13 14:34:49 +0100

    fix: default language is Dutch

--- src/Http/Logger/LoggerServiceProvider.php
+++ src/Http/Logger/LoggerServiceProvider.php
@@ -16,7 +16,7 @@ class LoggerServiceProvider extends ServiceProvider
         $this->setLoggingLevel();
 
         if ($this->isEnabled()) {
-            add_action('http_api_debug', [$this, 'logHttpMessage'], 10, 5);    
+            add_action('http_api_debug', [$this, 'logHttpMessage'], 10, 5);
         }
 
         add_filter('owc_gravityforms_zaaksysteem_gf_settings', [$this, 'addLoggingOptions']);
@@ -43,9 +43,9 @@ class LoggerServiceProvider extends ServiceProvider
         // Insert logging fields after the 'Algemene instellingen' fields.
         $columns = array_column($fields, 'title');
         $index = (array_flip($columns)['Algemene instellingen'] ?? 0) + 1;
-        
+
         return array_merge(
-            array_slice($fields, 0, $index), 
+            array_slice($fields, 0, $index),
             [$this->getFieldSettings()],
             array_slice($fields, $index, count($fields))
         );
@@ -54,42 +54,42 @@ class LoggerServiceProvider extends ServiceProvider
     protected function getFieldSettings(): array
     {
         return [
-            'title'  => esc_html__('Enable message logging', 'owc-gravityforms-zaaksysteem'),
+            'title'  => esc_html__('Berichtenverkeer logboek', 'owc-gravityforms-zaaksysteem'),
             'fields' => [
                 [
                     'name'    => "{$this->settingsPrefix}-form-setting-logging",
                     'default_value' => "0",
                     'tooltip' => sprintf(
                         '<h6>%s</h6>%s',
-                        __('Enable message logging', 'owc-gravityforms-zaaksysteem'),
-                        __('Enable logging all, some or no HTTP messages.', 'owc-gravityforms-zaaksysteem')
+                        __('Berichtenverkeer logboek', 'owc-gravityforms-zaaksysteem'),
+                        __('Activeer het loggen van het HTTP berichtenverkeer. Afhankelijk van het logniveau worden er meer of minder details bijgehouden.', 'owc-gravityforms-zaaksysteem')
                     ),
                     'type'    => 'select',
-                    'label'   => esc_html__('Select logging level', 'owc-gravityforms-zaaksysteem'),
+                    'label'   => esc_html__('Selecteer logboek granulariteit', 'owc-gravityforms-zaaksysteem'),
                     'choices' => [
                         [
                             'name'  => "{$this->settingsPrefix}-form-setting-logging-none",
-                            'label' => __('Disable logging', 'owc-gravityforms-zaaksysteem'),
+                            'label' => __('Deactiveer logboek', 'owc-gravityforms-zaaksysteem'),
                             'value' => '0',
                         ],
                         [
-                            'name'  => "{$this->settingsPrefix}-form-setting-logging-".MessageDetail::WHITE_BOX,
-                            'label' => __('White box', 'owc-gravityforms-zaaksysteem'),
+                            'name'  => "{$this->settingsPrefix}-form-setting-logging-" . MessageDetail::WHITE_BOX,
+                            'label' => __('Hoog (White box)', 'owc-gravityforms-zaaksysteem'),
                             'value' => MessageDetail::WHITE_BOX,
                         ],
                         [
-                            'name'  => "{$this->settingsPrefix}-form-setting-logging-".MessageDetail::GRAY_BOX,
-                            'label' => __('Gray box', 'owc-gravityforms-zaaksysteem'),
+                            'name'  => "{$this->settingsPrefix}-form-setting-logging-" . MessageDetail::GRAY_BOX,
+                            'label' => __('Gemiddeld (Gray box)', 'owc-gravityforms-zaaksysteem'),
                             'value' => MessageDetail::GRAY_BOX,
                         ],
                         [
-                            'name'  => "{$this->settingsPrefix}-form-setting-logging-".MessageDetail::BLACK_BOX,
-                            'label' => __('Black box', 'owc-gravityforms-zaaksysteem'),
+                            'name'  => "{$this->settingsPrefix}-form-setting-logging-" . MessageDetail::BLACK_BOX,
+                            'label' => __('Laag (Black box)', 'owc-gravityforms-zaaksysteem'),
                             'value' => MessageDetail::BLACK_BOX,
                         ],
                         [
-                            'name'  => "{$this->settingsPrefix}-form-setting-logging-".MessageDetail::URL_LOGGING,
-                            'label' => __('URL only', 'owc-gravityforms-zaaksysteem'),
+                            'name'  => "{$this->settingsPrefix}-form-setting-logging-" . MessageDetail::URL_LOGGING,
+                            'label' => __('Enkel URLs', 'owc-gravityforms-zaaksysteem'),
                             'value' => MessageDetail::URL_LOGGING,
                         ],
                     ],
