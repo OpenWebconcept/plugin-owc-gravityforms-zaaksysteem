2e2210de87bfdc2930b70adc7262d29a9774d5e3
Mike van den Hoek <mike@yard.nl>
2024-06-05 12:43:19 +0200

    (chore): remove readspeaker from plug-in views

--- resources/views/form-submission-pdf-failed.php
+++ resources/views/form-submission-pdf-failed.php
@@ -7,15 +7,6 @@ get_header(); ?>
                 <h1>
                     Er ging iets fout!
                 </h1>
-                <div id="readspeaker_button1" class="rs_skip rsbtn rs_preserve">
-                    <a rel="nofollow" class="rsbtn_play" accesskey="L"
-                        title="Laat de tekst voorlezen met ReadSpeaker webReader"
-                        href="//app-eu.readspeaker.com/cgi-bin/rsent?customerid=8150&amp;lang=nl_nl&amp;readid=readspeaker&amp;url=<?php echo get_permalink() ?>">
-                        <span class="rsbtn_left rsimg rspart"><span class="rsbtn_text"><span>Lees
-                                    voor</span></span></span>
-                        <span class="rsbtn_right rsimg rsplay rspart"></span>
-                    </a>
-                </div>
             </header>
             <div class="content">
                 <div id="readspeaker">
