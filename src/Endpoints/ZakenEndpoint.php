e4d2ad56ac5a658e573990e96c050923f2e21f6f
Mike van den Hoek <mike@yard.nl>
2024-12-04 13:43:00 +0100

    (fix): check if ->status->statustype isset before retrieving status explanation

--- src/Endpoints/ZakenEndpoint.php
+++ src/Endpoints/ZakenEndpoint.php
@@ -52,7 +52,7 @@ class ZakenEndpoint extends Endpoint
         $class = $this->entityClass;
         $zaak = new $class($data, $this->client::CALLABLE_NAME, $this->client::CLIENT_NAME);
 
-        $statusToelichting = $zaak->status instanceof Status ? $zaak->status->statustype->statusExplanation() : '';
+        $statusToelichting = $zaak->status instanceof Status && isset($zaak->status->statustype) ? $zaak->status->statustype->statusExplanation() : '';
         $zaak->setValue('leverancier', $zaak->getClientNamePretty());
         $zaak->setValue('steps', $this->handleProcessStatusses($this->getStatussenSorted($zaak), $statusToelichting));
         $zaak->setValue('status_history', $zaak->statussen);
