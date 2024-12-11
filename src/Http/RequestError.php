4c0a644e8a753e4236753e8b9d0904a9e61888f3
Mike van den Hoek <mike@yard.nl>
2024-12-11 17:00:27 +0100

    (refactor): format JSON response error message

--- src/Http/RequestError.php
+++ src/Http/RequestError.php
@@ -5,19 +5,22 @@ declare(strict_types=1);
 namespace OWC\Zaaksysteem\Http;
 
 use Exception;
+use Throwable;
 
 class RequestError extends Exception
 {
+    protected const DEFAULT_ERROR_MESSAGE = 'A request error occurred. Additionally, no error message could be retrieved.';
+
     protected ?Response $response = null;
 
     public static function fromResponse(Response $response)
     {
         try {
             $json = $response->getParsedJson();
-            $message = sprintf('%s "%s".', $json['title'] ?? '', $json['detail'] ?? '');
+            $message = (new static())->formatResponse($json);
             $status = $json['status'] ?? 0;
-        } catch (\Throwable $e) {
-            $message = 'A request error occurred. Additionally, no error message could be retrieved.';
+        } catch (Throwable $e) {
+            $message = self::DEFAULT_ERROR_MESSAGE;
             $status = 0;
         }
 
@@ -37,5 +40,14 @@ class RequestError extends Exception
     public function getResponse(): ?Response
     {
         return $this->response;
+    }
+
+    protected function formatResponse(array $json): string
+    {
+        if (isset($json['title']) && isset($json['detail'])) {
+            return sprintf('%s "%s".', $json['title'], $json['detail']);
+        }
+
+        return self::DEFAULT_ERROR_MESSAGE;
     }
 }
