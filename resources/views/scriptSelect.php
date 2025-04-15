d6986eebf0c91c9ed1ff461979dd39c1a4128c77
Mike van den Hoek <mike@yard.nl>
2025-04-15 16:58:53 +0200

    (feat): add 'zaak' document descriptions

--- resources/views/scriptSelect.php
+++ resources/views/scriptSelect.php
@@ -7,9 +7,14 @@
     // To display custom field under each type of Gravity Forms field.
     jQuery.each(fieldSettings, function(index, value) {
         fieldSettings[index] += ', .highlight_setting_zgw';
+
+		if (index === 'fileupload') {
+			fieldSettings[index] += ', .zgw_upload_setting';
+		}
     });
     jQuery(document).bind('gform_load_field_settings', function(event, field, form) {
         jQuery('#linkedFieldZGW').val(field['linkedFieldValueZGW']);
 		jQuery('#linkedFieldDocumentType').val(field['linkedFieldValueDocumentType']);
+		jQuery('#linkedUploadFieldDescriptionZGW').val(field['linkedUploadFieldDescriptionValueZGW']);
     });
 </script>
