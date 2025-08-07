2c70247f29ec65854cb20e0e30cc678a7882b65d
Mike van den Hoek <mike@yard.nl>
2025-08-07 12:59:42 +0200

    (refactor): mime mapping array from DI container

--- config/container.php
+++ config/container.php
@@ -386,4 +386,19 @@ return [
 
         return $logger;
     },
+
+    'mime.mapping' => function (Container $container) {
+        return [
+            'application/pdf' => 'pdf',
+            'application/msword' => 'doc',
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => 'docx',
+            'application/vnd.ms-excel' => 'xls',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' => 'xlsx',
+            'text/plain' => 'txt',
+            'text/csv' => 'csv',
+            'text/html' => 'html',
+            'application/json' => 'json',
+            'application/xml' => 'xml',
+        ];
+    },
 ];
